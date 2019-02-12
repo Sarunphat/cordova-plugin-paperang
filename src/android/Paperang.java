24ade8647705d5507337360019c41f41a67abb80
Sarunphat Pisutvimol <sarunphat@mfec.co.th>
2019-02-12 10:38:16 +0700

    [Sarunphat]

--- src/android/Paperang.java
+++ src/android/Paperang.java
@@ -9,6 +9,7 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import cn.paperang.sdk.btclient.callback.OnInitStatusListener;
 import cn.paperang.sdk.btclient.callback.OnBtDeviceListener;
 import cn.paperang.sdk.btclient.callback.OnBtStatusChangeListener;
 import cn.paperang.sdk.btclient.model.PaperangDevice;
@@ -22,6 +23,27 @@ public class Paperang extends CordovaPlugin {
 
     public static final String ACCESS_COARSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION;
     public static final int SEARCH_REQ_CODE = 0;
+
+    private Context mContext;
+
+    @Override
+    protected void pluginInitialize() {
+        mContext = cordova.getActivity();
+        Context appContext = mContext.getApplicationContext();
+        PaperangApi.init(this, appContext.getPackageName(), new OnInitStatusListener() {
+            @Override
+            public void initStatus(boolean b) {
+                Log.e("sys", "b = " + b);
+            }
+        });
+        PaperangApi.registerBT(appContext);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        PaperangApi.unregisterBT(this);
+    }
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
