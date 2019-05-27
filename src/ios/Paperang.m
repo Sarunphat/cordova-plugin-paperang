5ec8424890c24e0a00cb1f3b43f1f393d79f4b92
Sarunphat <sarunphat.pisutvimol@gmail.com>
2019-05-27 21:13:32 +0700

    Update Paperang.m

--- src/ios/Paperang.m
+++ src/ios/Paperang.m
@@ -59,11 +59,7 @@
 	    [MMSharePrint startScan];
         // Create timer to stop scanning
         double delayInSeconds = 5.0;
-        dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC));
-        dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
-            NSLog(@"Do some work");
-            [self didStopScanning];
-        });
+        [self performSelector:@selector(didStopScanning) withObject:nil afterDelay: delayInSeconds]
     }];
 }
 - (void) didDiscoverDevice:(NSNotification *)noti {
